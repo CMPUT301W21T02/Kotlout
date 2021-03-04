739b8f977a99253b82888e307ff6b5bec6420507
Dillon Allan <dlallan@users.noreply.github.com>
2021-03-04 16:31:17 -0700

    Finish Experiment class

--- app/src/main/java/xyz/kotlout/kotlout/model/experiment/Experiment.java
+++ app/src/main/java/xyz/kotlout/kotlout/model/experiment/Experiment.java
@@ -1,12 +1,16 @@
 package xyz.kotlout.kotlout.model.experiment;
 
+import java.util.List;
+
+import xyz.kotlout.kotlout.model.user.User;
+
 public abstract class Experiment {
-//    private User owner; // TODO
+    private User owner;
     private String description;
     private String region;
     private int minimumTrials;
     private boolean isOngoing;
     private boolean geolocationRequired;
-//    private List<User> ignoredUsers; // TODO
-//    private List<Post> posts; // TODO
+    private List<User> ignoredUsers;
+    private List<Post> posts;
 }
