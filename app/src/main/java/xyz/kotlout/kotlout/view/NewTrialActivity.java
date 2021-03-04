7b5f5d6e1cc2979afdd401a65305e96004d43ef0
Michael Kwok <michael.kwok00@gmail.com>
2021-03-04 16:00:19 -0700

    Add skeleton files for all user stories

---
+++ app/src/main/java/xyz/kotlout/kotlout/view/NewTrialActivity.java
@@ -0,0 +1,16 @@
+package xyz.kotlout.kotlout.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import xyz.kotlout.kotlout.R;
+
+public class NewTrialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
