0da91f3ded4ff881428ffee1c8065c4e8bd5a3cc
Dillon Allan <dlallan@users.noreply.github.com>
2021-03-04 16:14:17 -0700

    Add all Experiment and Trial classes

---
+++ app/src/main/java/xyz/kotlout/kotlout/model/experiment/CountExperiment.java
@@ -0,0 +1,9 @@
+package xyz.kotlout.kotlout.model.experiment;
+
+import java.util.List;
+
+import xyz.kotlout.kotlout.model.experiment.trial.CountTrial;
+
+public class CountExperiment extends Experiment {
+    private List<CountTrial> trials;
+}
