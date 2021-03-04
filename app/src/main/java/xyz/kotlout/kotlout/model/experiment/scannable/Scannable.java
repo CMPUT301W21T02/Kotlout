4bbef4eb1aeccf9f0421222651cb38863179e52c
Dillon Allan <dlallan@users.noreply.github.com>
2021-03-04 16:18:35 -0700

    Add all Scannable model classes

---
+++ app/src/main/java/xyz/kotlout/kotlout/model/experiment/scannable/Scannable.java
@@ -0,0 +1,7 @@
+package xyz.kotlout.kotlout.model.experiment.scannable;
+
+public class Scannable {
+    private int experimentId;
+//    private Geolocation geolocation;
+    private Object outcome;
+}
