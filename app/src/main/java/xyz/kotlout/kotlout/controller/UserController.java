a7a5853abf02365e4fd396dee100d0503b7502c5
Amir Hansen <mia2b@hotmail.com>
2021-03-11 15:14:37 -0700

    UserController Firebase interfacing

--- app/src/main/java/xyz/kotlout/kotlout/controller/UserController.java
+++ app/src/main/java/xyz/kotlout/kotlout/controller/UserController.java
@@ -1,10 +1,17 @@
 package xyz.kotlout.kotlout.controller;
 
+import android.util.Log;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.regex.Pattern;
 import xyz.kotlout.kotlout.model.user.User;
 
 public class UserController {
-
+  private static final String USER_COLLECTION = "users";
   /**
    * Pattern to validate email addresses </br> Regex from: https://emailregex.com, Accessed on
    * Friday March 5th 2021
@@ -20,12 +27,57 @@ public class UserController {
 
   private User user;
 
+  private void syncUser() {
+    FirebaseFirestore firestore = FirebaseController.getFirestore();
+    firestore.collection(USER_COLLECTION).document(this.user.getUuid()).set(this.user);
+  }
+
   public static boolean validateEmail(String email) {
     return EMAIL_REGEX.matcher(email).matches();
   }
 
   public static boolean validatePhoneNumber(String phoneNumber) {
     return PHONE_REGEX.matcher(phoneNumber).matches();
+  }
+
+  public static void registerUser(String userUUID) {
+    if (fetchUser(userUUID) == null) {
+      User newUser = new User();
+      newUser.setUuid(userUUID);
+      FirebaseFirestore firestore = FirebaseController.getFirestore();
+      firestore.collection(USER_COLLECTION).document(newUser.getUuid()).set(newUser);
+    } else {
+      throw new RuntimeException("User already registered!");
+    }
+  }
+
+  public static User fetchUser(String userUUID) {
+    FirebaseFirestore firestore = FirebaseController.getFirestore();
+    final CollectionReference collection = firestore.collection(USER_COLLECTION);
+    final User[] readUser = {null};
+    collection.document(userUUID).get().addOnCompleteListener(snapshot -> {
+      if (snapshot.isSuccessful()) {
+        DocumentSnapshot document = snapshot.getResult();
+        if (document.exists()) {
+          Log.d("FIRESTORE", "Document snapshot: " + document.getData());
+          readUser[0] = (User) document.toObject(User.class);
+        } else {
+          Log.d("FIRESTORE", "Document snapshot not found");
+        }
+      } else {
+        Log.d("FIRESTORE", "Get failed with: " + snapshot.getException());
+      }
+    });
+    return readUser[0];
+  }
+
+  public void updateUser(User user) {
+    if (this.user.getUuid() == user.getUuid()) {
+      this.user = user; //TODO: This probably needs changed for only updating say user info
+      syncUser();
+    } else {
+      throw new RuntimeException("UUIDs don't match to update user");
+    }
   }
 
   public User getUser() {
