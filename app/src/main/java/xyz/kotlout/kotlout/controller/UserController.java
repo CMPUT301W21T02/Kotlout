6b2b14a20f8ae94600da95873eea5b07778f155d
Tharidu Witharana <twitharana@gmail.com>
2021-03-12 00:59:30 -0700

    Added Javadoc comments

--- app/src/main/java/xyz/kotlout/kotlout/controller/UserController.java
+++ app/src/main/java/xyz/kotlout/kotlout/controller/UserController.java
@@ -6,6 +6,7 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
 import java.util.regex.Pattern;
 import xyz.kotlout.kotlout.model.user.User;
 
@@ -25,24 +26,17 @@ public class UserController {
   private static final Pattern PHONE_REGEX = Pattern.compile("\\d{3}-\\d{3}-\\d{4}");
   private static final String TAG = "USER CONTROLLER";
   private final DocumentReference userDoc;
+  private ListenerRegistration snapshotListenerRef;
   private Consumer<User> updateCallback;
   private User user;
 
+  /**
+   * Creates a UserController with a Firestore Document Reference a given user doc
+   * @param userId Id of the userdoc to reference with this controller
+   */
   public UserController(String userId) {
     userDoc = FirebaseController.getFirestore().collection(USER_COLLECTION).document(userId);
-    userDoc.addSnapshotListener((value, error) -> {
-      Log.d(TAG, "Firebase event");
-      if (error != null) {
-        Log.d(TAG, "Firebase Error: " + error.getMessage());
-        return;
-      }
-      if (value != null && value.exists()) {
-        Log.d(TAG, "FOUND VALID UPDATE");
-        updateCallback.accept(value.toObject(User.class));
-      } else {
-        Log.d(TAG, "no update");
-      }
-    });
+    registerSnapshotListener();
   }
 
   public static boolean validateEmail(String email) {
@@ -106,6 +100,42 @@ public class UserController {
     return user[0];
   }
 
+  /**
+   * Creates a snapshot listener for the userdoc iff snapshotListenerRef does not
+   * already have a listener. The new listener is stored in snapshotListenerRef
+   * Unregister this listener with unregisterSnapshotListener
+   * when finished using unregisterSnapshotListener()
+   */
+  public void registerSnapshotListener() {
+    if (snapshotListenerRef == null) {
+      snapshotListenerRef =
+          userDoc.addSnapshotListener((value, error) -> {
+            Log.d(TAG, "Firebase event");
+            if (error != null) {
+              Log.d(TAG, "Firebase Error: " + error.getMessage());
+              return;
+            }
+            if (value != null && value.exists()) {
+              Log.d(TAG, "FOUND VALID UPDATE");
+              updateCallback.accept(value.toObject(User.class));
+            } else {
+              Log.d(TAG, "no update");
+            }
+          });
+    }
+  }
+
+  /**
+   * Unregisters snapshotListenerRef and sets it to null
+   * Should be called once listener is no longer needed
+   */
+  public void unregisterSnapshotListener() {
+    if(snapshotListenerRef != null) {
+      snapshotListenerRef.remove();
+    }
+    snapshotListenerRef = null;
+  }
+
   private void syncUser() {
     FirebaseFirestore firestore = FirebaseController.getFirestore();
     firestore.collection(USER_COLLECTION).document(this.user.getUuid()).set(this.user);
@@ -120,6 +150,10 @@ public class UserController {
     }
   }
 
+  /**
+   * Sets the callback to be run when user document changes
+   * @param updateCallback
+   */
   public void setUpdateCallback(Consumer<User> updateCallback) {
     this.updateCallback = updateCallback;
   }
