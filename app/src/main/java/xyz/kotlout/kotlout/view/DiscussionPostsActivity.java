7fc4497b86708233cd07c14dc82814e2d1186641
AssortedFantasy <jehanzeb.mirza@yahoo.com>
2021-03-16 15:16:37 -0400

    Created an Acitivty and layout for Discussions

---
+++ app/src/main/java/xyz/kotlout/kotlout/view/DiscussionPostsActivity.java
@@ -0,0 +1,15 @@
+package xyz.kotlout.kotlout.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import xyz.kotlout.kotlout.R;
+
+public class DiscussionPostsActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_discussion_posts);
+
+  }
+}
