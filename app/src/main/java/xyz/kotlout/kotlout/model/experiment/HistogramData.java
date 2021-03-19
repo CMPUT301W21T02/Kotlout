1234238a4499fee4ba831cad020f6b771e6f41d7
Anmol Dhaliwal <AnmolDhaliwal00@users.noreply.github.com>
2021-03-19 14:48:14 -0600

    Histograms which load dummy data have been added

---
+++ app/src/main/java/xyz/kotlout/kotlout/model/experiment/HistogramData.java
@@ -0,0 +1,61 @@
+package xyz.kotlout.kotlout.model.experiment;
+
+import java.util.Objects;
+
+/**
+ * Base class for data points which are needed to create histograms with the MPGraph library
+ */
+public class HistogramData {
+  String result;
+  int count;
+
+  /**
+   * Creates a new data point
+   * @param result The result of the trial (e.g. Success, Failure, some measurement)
+   * @param count The amount of that specific result
+   */
+  public HistogramData(String result, int count) {
+    this.result = result;
+    this.count = count;
+  }
+
+  /**
+   * Gets the result of the trial
+   * @return A string containing the result of the trial
+   */
+  public String getResult() {
+    return result;
+  }
+
+  /**
+   * Gets the count of the trial
+   * @return an integer representing the count of the trial
+   */
+  public int getCount() {
+    return count;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    HistogramData that = (HistogramData) obj;
+    return Objects.equals(this.result, that.result);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(result);
+  }
+
+  /**
+   * Merges two histogram data points by combining their count
+   * @param other another instance of HistogramData
+   * @return A new HistogramData object with the counts of the two HistogramData objects combined, and the same result as both objects
+   */
+  public HistogramData merge(HistogramData other) {
+    assert(this.equals(other));
+    return new HistogramData(this.result, this.count + other.count);
+  }
+
+}
--- app/src/main/java/xyz/kotlout/kotlout/view/fragment/ExperimentInfoFragment.java
+++ app/src/main/java/xyz/kotlout/kotlout/view/fragment/ExperimentInfoFragment.java
@@ -5,18 +5,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import java.util.ArrayList;
 import xyz.kotlout.kotlout.R;
 import xyz.kotlout.kotlout.model.ExperimentType;
 import xyz.kotlout.kotlout.model.experiment.Experiment;
+import xyz.kotlout.kotlout.model.experiment.HistogramData;
 import xyz.kotlout.kotlout.view.InfoHeaderView;
 
+/**
+ * Provides information about the current trial
+ */
 public class ExperimentInfoFragment extends Fragment {
 
   private static final String ARG_EXPERIMENT = "EXPERIMENT";
   private static final String ARG_EXPERIMENT_TYPE = "EXPERIMENT_TYPE";
 
+  // Declaration of objects
   Experiment experiment;
   ExperimentType type;
+  BarChart histogram;
+  ArrayList<BarEntry> barEntries;
+  ArrayList<String> labels;
+
+  ArrayList<HistogramData> histogramData = new ArrayList<>();
 
   public static ExperimentInfoFragment newInstance(Experiment experiment, ExperimentType type) {
     ExperimentInfoFragment fragment = new ExperimentInfoFragment();
@@ -44,7 +64,91 @@ public class ExperimentInfoFragment extends Fragment {
 
     InfoHeaderView infoHeader = view.findViewById(R.id.ihv_experiment_info);
     infoHeader.setExperiment(experiment, type);
+    
+    histogram = view.findViewById(R.id.histogram);
+    barEntries = new ArrayList<>();
+    labels = new ArrayList<>();
+
+    fillData();
+    ArrayList<HistogramData> merged = mergeData(histogramData);
+    FormatHistogram(histogram);
 
     return view;
+  }
+
+  /**
+   * Formats the axes and bars of the histogram
+   * @param histogram the histogram with the data loaded in
+   */
+  private void FormatHistogram(BarChart histogram) {
+    BarDataSet barDataSet = new BarDataSet(barEntries, "Answers");
+    barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+    Description description = new Description();
+    description.setText("Answers");
+    histogram.setDescription(description);
+    BarData barData = new BarData(barDataSet);
+    histogram.setData(barData);
+
+    XAxis xAxis = histogram.getXAxis();
+    xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+    xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+    xAxis.setDrawGridLines(false);
+    xAxis.setDrawAxisLine(false);
+    xAxis.setGranularity(1f);
+    xAxis.setLabelCount(labels.size());
+    xAxis.setLabelRotationAngle(0);
+    YAxis left = histogram.getAxisLeft();
+    YAxis right = histogram.getAxisRight();
+    left.setAxisMinimum(0);
+    right.setAxisMinimum(0);
+    histogram.animateY(1000);
+    histogram.invalidate();
+    histogram.setScaleY(1);
+  }
+
+  // Loads dummy data for now as we don't have trials set-up yet
+  private void fillData() {
+    histogramData.add(new HistogramData("Success", 1));
+    histogramData.add(new HistogramData("Success", 1));
+    histogramData.add(new HistogramData("Success", 1));
+    histogramData.add(new HistogramData("Success", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Success", 1));
+    histogramData.add(new HistogramData("Success", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+  }
+
+  /**
+   * Merges the counts of all trials with the same results
+   * @param histogramData The list of all trial results
+   * @return A list of the results that are merged if they have the same result
+   */
+  public ArrayList<HistogramData> mergeData(ArrayList<HistogramData> histogramData) {
+    ArrayList<HistogramData> merged = new ArrayList<>();
+    for (HistogramData h : histogramData) {
+      int index = merged.indexOf(h);
+      if(index != -1) {
+        merged.set(index, merged.get(index).merge(h));
+      } else {
+        merged.add(h);
+      }
+    }
+
+    for (int i = 0; i < merged.size(); i++) {
+      String date = merged.get(i).getResult();
+      int amount = merged.get(i).getCount();
+
+      barEntries.add(new BarEntry(i, amount));
+      labels.add(date);
+    }
+
+    return  merged;
   }
 }
