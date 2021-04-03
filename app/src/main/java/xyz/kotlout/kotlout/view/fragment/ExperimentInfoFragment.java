742303946c873fe293987bd1bbdc0dcdb218c880
Anmol Dhaliwal <AnmolDhaliwal00@users.noreply.github.com>
2021-04-03 13:06:59 -0600

    Logic has been added to create histograms what aggregate trial results by data

--- app/src/main/java/xyz/kotlout/kotlout/view/fragment/ExperimentInfoFragment.java
+++ app/src/main/java/xyz/kotlout/kotlout/view/fragment/ExperimentInfoFragment.java
@@ -6,14 +6,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.ScatterChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.ScatterData;
+import com.github.mikephil.charting.data.ScatterDataSet;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.type.Color;
 import java.util.ArrayList;
 import xyz.kotlout.kotlout.R;
 import xyz.kotlout.kotlout.model.ExperimentType;
@@ -32,11 +36,12 @@ public class ExperimentInfoFragment extends Fragment {
   // Declaration of objects
   Experiment experiment;
   ExperimentType type;
-  BarChart histogram;
-  ArrayList<BarEntry> barEntries;
-  ArrayList<String> labels;
+  BarChart histogram, trialInfo;
+  ArrayList<BarEntry> barEntries, trialEntries;
+  ArrayList<String> labels, trialLabels;
 
   ArrayList<HistogramData> histogramData = new ArrayList<>();
+  ArrayList<HistogramData> trialData = new ArrayList<>();
 
   public static ExperimentInfoFragment newInstance(Experiment experiment, ExperimentType type) {
     ExperimentInfoFragment fragment = new ExperimentInfoFragment();
@@ -73,7 +78,57 @@ public class ExperimentInfoFragment extends Fragment {
     ArrayList<HistogramData> merged = mergeData(histogramData);
     FormatHistogram(histogram);
 
+    //------------------------------------------
+    trialInfo = view.findViewById(R.id.trialInfo);
+    trialEntries = new ArrayList<>();
+    trialLabels = new ArrayList<>();
+
+    fillEntries();
+
+    FormatNonNegMeasurement(trialInfo);
+
+//    switch (ARG_EXPERIMENT_TYPE){
+//      case "EXPERIMENT":
+//        FormatCountNonNegMeasurement(trialInfo);
+//      case "BINOMIAL":
+//    }
+
+
     return view;
+  }
+
+  private void fillTrialData(){
+
+  }
+
+  private void FormatBinomial (BarChart trialInfo){
+
+  }
+
+  private void FormatNonNegMeasurement (BarChart histogram){
+    BarDataSet barDataSet = new BarDataSet(trialEntries, "Means");
+    barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+    Description description = new Description();
+    description.setText("Means");
+    histogram.setDescription(description);
+    BarData barData = new BarData(barDataSet);
+    histogram.setData(barData);
+
+    XAxis xAxis = histogram.getXAxis();
+    xAxis.setValueFormatter(new IndexAxisValueFormatter(trialLabels));
+    xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+    xAxis.setDrawGridLines(false);
+    xAxis.setDrawAxisLine(false);
+    xAxis.setGranularity(1f);
+    xAxis.setLabelCount(trialLabels.size());
+    xAxis.setLabelRotationAngle(0);
+    YAxis left = histogram.getAxisLeft();
+    YAxis right = histogram.getAxisRight();
+    left.setAxisMinimum(0);
+    right.setAxisMinimum(0);
+    histogram.animateY(1000);
+    histogram.invalidate();
+    histogram.setScaleY(1);
   }
 
   /**
@@ -106,23 +161,6 @@ public class ExperimentInfoFragment extends Fragment {
     histogram.setScaleY(1);
   }
 
-  // Loads dummy data for now as we don't have trials set-up yet
-  private void fillData() {
-    histogramData.add(new HistogramData("Success", 1));
-    histogramData.add(new HistogramData("Success", 1));
-    histogramData.add(new HistogramData("Failure", 1));
-    histogramData.add(new HistogramData("Failure", 1));
-    histogramData.add(new HistogramData("Failure", 1));
-    histogramData.add(new HistogramData("Failure", 1));
-    histogramData.add(new HistogramData("Failure", 1));
-    histogramData.add(new HistogramData("Failure", 1));
-    histogramData.add(new HistogramData("Failure", 1));
-    histogramData.add(new HistogramData("Success", 1));
-    histogramData.add(new HistogramData("Success", 1));
-    histogramData.add(new HistogramData("Failure", 1));
-    histogramData.add(new HistogramData("Failure", 1));
-  }
-
   /**
    * Merges the counts of all trials with the same results
    * @param histogramData The list of all trial results
@@ -147,6 +185,41 @@ public class ExperimentInfoFragment extends Fragment {
       labels.add(date);
     }
 
-    return  merged;
+    return merged;
+  }
+
+  // Loads dummy data for now as we don't have trials set-up yet
+  private void fillEntries() {
+    trialData.add(new HistogramData("2021-01-05", 56));
+    trialData.add(new HistogramData("2021-01-06", 63));
+    trialData.add(new HistogramData("2021-01-07", 72));
+    trialData.add(new HistogramData("2021-01-08", 97));
+    trialData.add(new HistogramData("2021-01-09", 12));
+    trialData.add(new HistogramData("2021-01-10", 55));
+
+    for (int i = 0; i < trialData.size(); i++) {
+      String date = trialData.get(i).getResult();
+      int amount = trialData.get(i).getCount();
+
+      trialEntries.add(new BarEntry(i, amount));
+      trialLabels.add(date);
+    }
+  }
+
+  // Loads dummy data for now as we don't have trials set-up yet
+  private void fillData() {
+    histogramData.add(new HistogramData("Success", 1));
+    histogramData.add(new HistogramData("Success", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Success", 1));
+    histogramData.add(new HistogramData("Success", 1));
+    histogramData.add(new HistogramData("Failure", 1));
+    histogramData.add(new HistogramData("Failure", 1));
   }
 }
