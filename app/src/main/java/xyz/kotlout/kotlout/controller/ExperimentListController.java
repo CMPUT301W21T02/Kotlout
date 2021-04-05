2418a2760a610742518a211703399c5e705d3c07
Dillon Allan <dlallan@users.noreply.github.com>
2021-04-04 21:15:07 -0600

    Remove initializeMyExperiments() and do some clean-up

--- app/src/main/java/xyz/kotlout/kotlout/controller/ExperimentListController.java
+++ app/src/main/java/xyz/kotlout/kotlout/controller/ExperimentListController.java
@@ -3,10 +3,6 @@ package xyz.kotlout.kotlout.controller;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * A controller for managing lists of Experiment objects.
@@ -14,31 +10,25 @@ import java.util.Map;
 public class ExperimentListController {
 
   private static final String TAG = "EXP_LIST_CONTROLLER";
-  private final Query getUserExperiments;
+  private final String userId;
 
-  public ExperimentListController(String userUuid) {
-    FirebaseFirestore db = FirebaseController.getFirestore();
-    CollectionReference experimentsRef = db.collection(ExperimentController.EXPERIMENT_COLLECTION);
-    getUserExperiments = experimentsRef.whereEqualTo("ownerUuid", userUuid);
+  /**
+   * Initializes the controller with the given user ID.
+   * @param userId The user ID.
+   */
+  public ExperimentListController(String userId) {
+    this.userId = userId;
   }
 
   /**
-   * Initialize the list for My Experiments
+   * Gets a query for getting all experiments owned by the given user.
    *
-   * @return An empty list for user experiments grouped by Open and Closed.
+   * @return A instance of Query for getting all experiments for the given user.
    */
-  public Map<MyExperimentGroup, List<ExperimentController>> initializeMyExperiments() {
-
-    Map<MyExperimentGroup, List<ExperimentController>> myExperiments = new HashMap<>();
-
-    for (MyExperimentGroup group : MyExperimentGroup.values()) {
-      myExperiments.put(group, new ArrayList<>());
-    }
-    return myExperiments;
-  }
-
-  public Query getGetUserExperiments() {
-    return getUserExperiments;
+  public Query getUserExperiments() {
+    FirebaseFirestore db = FirebaseController.getFirestore();
+    CollectionReference experimentsRef = db.collection(ExperimentController.EXPERIMENT_COLLECTION);
+    return experimentsRef.whereEqualTo("ownerUuid", this.userId);
   }
 
   /**
