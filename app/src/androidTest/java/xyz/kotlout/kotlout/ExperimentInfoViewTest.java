865969c2228878028a2d099a3c116d1af6513915
Anmol Dhaliwal <AnmolDhaliwal00@users.noreply.github.com>
2021-04-09 00:05:21 -0600

    Measurement trial test added

--- app/src/androidTest/java/xyz/kotlout/kotlout/ExperimentInfoViewTest.java
+++ app/src/androidTest/java/xyz/kotlout/kotlout/ExperimentInfoViewTest.java
@@ -102,4 +102,44 @@ public class ExperimentInfoViewTest {
     onView(withId(R.id.btn_new_trial_submit)).perform(click());
 
   }
+
+  @Test
+  public void testAddMeasurementTrial() {
+
+    String description = "How far did you walk today?";
+    String region = "Edmonton";
+    String minimumTrials = "10";
+    String typeOption = "Measurement";
+
+    // Open the add experiment fragment
+    onView(withId(R.id.fab_main_add_experiment))
+        .perform(click());
+
+    // Type in experiment details
+    onView(withId(R.id.et_experiment_new_description))
+        .perform(typeText(description));
+    onView(withId(R.id.et_experiment_new_region))
+        .perform(typeText(region));
+    onView(withId(R.id.et_experiment_new_min_trials))
+        .perform(typeText(minimumTrials), closeSoftKeyboard());
+
+    onView(withId(R.id.sp_experiment_new_type))
+        .perform(click());
+    onView(withText(typeOption)).perform(click());
+
+    // Submit
+    onView(withId(R.id.btn_experiment_new_add)).perform(click());
+    onView(withText("Open Experiments")).perform(click());
+
+    onView(withText(description)).perform(click());
+
+    onView(withText("TRIALS")).perform(click());
+
+    onView(withId(R.id.fab_view_add_trial)).perform(click());
+
+    onView((withId(R.id.editTextNumber))).perform(typeText("10"));
+
+    onView(withId(R.id.btn_new_trial_submit)).perform(click());
+
+  }
 }
