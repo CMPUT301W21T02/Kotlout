35662fc4577b51fc7eb1c20c95620bfb8a2a6c30
Michael Kwok <michael.kwok00@gmail.com>
2021-04-09 15:12:21 -0600

    update uml

--- app/src/main/java/xyz/kotlout/kotlout/model/experiment/scannable/Barcode.java
+++
@@ -1,9 +0,0 @@
-package xyz.kotlout.kotlout.model.experiment.scannable;
-
-/**
- * Barcode is a specific scan of a barcode done
- */
-public class Barcode extends Scannable {
-
-  private int value;
-}
