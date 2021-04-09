2e09b68f40c72206082025ec6158d4f5355b9707
Anmol Dhaliwal <AnmolDhaliwal00@users.noreply.github.com>
2021-04-08 23:58:43 -0600

    Test added for binomial trials

--- app/src/androidTest/java/xyz/kotlout/kotlout/ExperimentInfoViewTest.java
+++ app/src/androidTest/java/xyz/kotlout/kotlout/ExperimentInfoViewTest.java
@@ -23,7 +23,6 @@ public class ExperimentInfoViewTest {
   public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(
       MainActivity.class);
 
-
   @Test
   public void testAddCountTrial() {
 
@@ -59,6 +58,46 @@ public class ExperimentInfoViewTest {
     onView(withId(R.id.fab_view_add_trial)).perform(click());
 
     onView((withId(R.id.editTextNumber))).perform(typeText("1"));
+
+    onView(withId(R.id.btn_new_trial_submit)).perform(click());
+
+  }
+
+  @Test
+  public void testAddBinomialTrial() {
+
+    String description = "Did you see me today?";
+    String region = "Edmonton";
+    String minimumTrials = "10";
+    String typeOption = "Binomial";
+
+    // Open the add experiment fragment
+    onView(withId(R.id.fab_main_add_experiment))
+        .perform(click());
+
+    // Type in experiment details
+    onView(withId(R.id.et_experiment_new_description))
+        .perform(typeText(description));
+    onView(withId(R.id.et_experiment_new_region))
+        .perform(typeText(region));
+    onView(withId(R.id.et_experiment_new_min_trials))
+        .perform(typeText(minimumTrials), closeSoftKeyboard());
+
+    onView(withId(R.id.sp_experiment_new_type))
+        .perform(click());
+    onView(withText(typeOption)).perform(click());
+
+    // Submit
+    onView(withId(R.id.btn_experiment_new_add)).perform(click());
+    onView(withText("Open Experiments")).perform(click());
+
+    onView(withText(description)).perform(click());
+
+    onView(withText("TRIALS")).perform(click());
+
+    onView(withId(R.id.fab_view_add_trial)).perform(click());
+
+    onView((withId(R.id.radio_success))).perform(click());
 
     onView(withId(R.id.btn_new_trial_submit)).perform(click());
 
